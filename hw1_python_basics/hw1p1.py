88425b856a7e9ba0f4919006bb300bccfc336a07
dathray <78174160+dathray@users.noreply.github.com>
2021-02-02 15:10:44 -0500

    Add files via upload

--- hw1_python_basics/hw1p1.py
+++ hw1_python_basics/hw1p1.py
@@ -1,10 +1,35 @@
-#!/usr/bin/env python
+import matplotlib.pyplot as plt
+import numpy as np
+
+import person
 
 list_of_names = ['Roger', 'Mary', 'Luisa', 'Elvis']
 list_of_ages  = [23, 24, 19, 86]
 list_of_heights_cm = [175, 162, 178, 182]
 
 for name in list_of_names:
-  print("The name {:} is {:} letters long".format(name, len(name)))
-  
+    print("The name {:} is {:} letters long".format(name, len(name)))
+    
+list_length_name = [len(x) for x in list_of_names]
 
+people = {}
+ 
+for i in range(len(list_of_names)):
+    people[list_of_names[i]] = person.Person(name = list_of_names[i], age = list_of_ages[i],height = list_of_heights_cm[i])
+
+array_ages = np.array(list_of_ages)
+array_heights_cm = np.array(list_of_heights_cm)
+
+mean_ages = np.mean(array_ages)
+mean_heights_cm = np.mean(array_heights_cm)
+
+print("The average age = {:}".format(mean_ages))
+print("The average height in cm  = {:}".format(mean_heights_cm))
+
+plt.title("Age and Height in cm")
+plt.scatter(array_ages, array_heights_cm)
+plt.xlabel("Age")
+plt.ylabel("Height in cm")
+plt.grid(True)
+plt.savefig("./age_height_cm_plot.png")
+plt.show()
---
+++ hw1_python_basics/person.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age, height):
+        self.name = name
+        self.age = age
+        self.height = height
+    
+
+    def __repr__(self):
+        return "{:} is {:} years old.".format(new_person.name,new_person.age)
+        
+
+    
+        
